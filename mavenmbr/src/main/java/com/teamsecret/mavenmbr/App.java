a389fea0b1669783b769487f8e796d43216cc2a4
SebastianRauReyes <sebastian.rau@tecsup.edu.pe>
2018-02-19 12:01:38 -0500

    Integrando MAVEN

---
+++ mavenmbr/src/main/java/com/teamsecret/mavenmbr/App.java
@@ -0,0 +1,13 @@
+package com.teamsecret.mavenmbr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Team Secret!" );
+    }
+}
